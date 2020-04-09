ac72954231e4252d42e737fc6601c6ffad9b28cf
ARNJNHLLRO <arenjan19@gmail.com>
2020-04-09 15:11:06 +0800

    Updates

---
+++ precache-manifest.2052eeda1021fa5c3ab3f265fc71f3b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35823dd9563df22c1e0a856b1027651",
+    "url": "/armorymotogears/index.html"
+  },
+  {
+    "revision": "3946536ded754382a72d",
+    "url": "/armorymotogears/static/css/main.5c139dfe.chunk.css"
+  },
+  {
+    "revision": "9c0527294b5506e8993c",
+    "url": "/armorymotogears/static/js/2.3f797365.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/armorymotogears/static/js/2.3f797365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3946536ded754382a72d",
+    "url": "/armorymotogears/static/js/main.ccfc6229.chunk.js"
+  },
+  {
+    "revision": "7745f7982318ff492ca9",
+    "url": "/armorymotogears/static/js/runtime-main.b9b5e8dc.js"
+  },
+  {
+    "revision": "f0c61f8549bd158ff00409b6972cadeb",
+    "url": "/armorymotogears/static/media/armory-red.f0c61f85.png"
+  },
+  {
+    "revision": "1d72087d62939e6268ceae72509caaa4",
+    "url": "/armorymotogears/static/media/home-image.1d72087d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/armorymotogears/static/media/logo.5d5d9eef.svg"
+  }
+]);
